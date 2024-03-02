d8d17d1594cad8dc316316f14e7d9e55e913a26e
zexcv <krusher13@o2.pl>
2024-03-02 16:21:00 +0100

    React-slick Slider

--- my-page/src/components/Projects/AllProjects.jsx
+++ my-page/src/components/Projects/AllProjects.jsx
@@ -1,44 +1,38 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useRef } from "react";
 import "../../css/all-projects.component.css";
 import Project from "./Project.jsx";
 import { PROJECTS } from "../../utils/data.js";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Slider from "react-slick";
 
 const AllProjects = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [currentProject, setCurrentProject] = useState(0);
 
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const projectsToShow = windowWidth < 1025 ? 1 : 3;
-
-  const nextProject = () => {
-    setCurrentProject((prevProject) => (prevProject + 1) % PROJECTS.length);
-  };
-
-  const prevProject = () => {
-    setCurrentProject((prevProject) =>
-      prevProject === 0 ? PROJECTS.length - 1 : prevProject - 1
-    );
+  const sliderRef = useRef();
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 0,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
+        },
+      },
+    ],
   };
 
   return (
     <section className="all-projects-container">
       <h4>Personal Projects</h4>
-      <div className="projects">
-        {PROJECTS.slice(currentProject, currentProject + projectsToShow).map(
-          (project) => (
+      <h3>Swipe to see more</h3>
+        <Slider ref={sliderRef} {...settings}>
+          {PROJECTS.map((project) => (
             <Project
               key={project.title}
               title={project.title}
@@ -47,15 +41,8 @@ const AllProjects = () => {
               poster={project.poster}
               git={project.git}
             />
-          )
-        )}
-        {windowWidth < 1025 && (
-          <div className="project-navigation">
-            <button className="bn30" onClick={prevProject}><FontAwesomeIcon icon={faArrowLeft} /></button>
-            <button className="bn30" onClick={nextProject}><FontAwesomeIcon icon={faArrowRight} /></button>
-          </div>
-        )}
-      </div>
+          ))}
+        </Slider>
     </section>
   );
 };
